09addd040aefe07197ce37cf130994f49b87ff08
MiguelCalha <miguel.calha@estudantes.ips.pt>
2024-04-01 15:24:42 +0100

    Filter in Tutors WORKING
    Filter in Tutors WORKING

--- Tutor/TopTutor/obj/Debug/net8.0/TopTutor.AssemblyInfo.cs
+++ Tutor/TopTutor/obj/Debug/net8.0/TopTutor.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TopTutor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6279e6bbca4d5903b56c463a0f401ffc7bb5073f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc610344cedea189fb9551a1b562869bd503d2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TopTutor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TopTutor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
